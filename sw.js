926e9b92bff85c858db5b4c230ffbcbf8086b9fc
muhsinyarba <muhsinyarba@gmail.com>
2026-04-07 15:46:07 +0300

    Update service worker to version 3 and refactor code

--- sw.js
+++ sw.js
@@ -1,28 +1,20 @@
-const CACHE = 'ogretmen-takip-v1';
-// GitHub'daki gerçek dosya isimlerine göre güncellendi
-const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './ikon.png',
-  './android-chrome-192x192.png',
-  './android-chrome-512x512.png'
-];
+const CACHE_NAME = 'ogretmen-takip-v3'; // Sürümü v3 yaparak tüm eski verileri siliyoruz
+const ASSETS = ['./', './index.html', './manifest.json', './android-chrome-192x192.png', './android-chrome-512x512.png'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
+    })
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
